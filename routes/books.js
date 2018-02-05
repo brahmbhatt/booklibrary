a6dcdd5cc5d77a84100dc1edf079215715f1197e
brahmbhatt <margibrahmbhatt13@gmail.com>
2018-02-05 12:21:49 +0530

    added testcase for checking the connection with API 2

--- routes/books.js
+++ routes/books.js
@@ -1,26 +1,65 @@
-// const Models = require('../models');
 const https = require('https');
 
-module.exports = [{
+module.exports = [
+    {
+        method: 'GET',
+        path: '/books',
+        // path: 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks',
+        handler: (request, reply)=>{
+            let str = '';
+            let jsonObject = {};
+            https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (response)=>{
+                response.setEncoding('utf8');
+                response.on('data', (data)=>{
+                    str = str + data;
+                });
+                response.on('end', ()=>{
+                    reply({
+                        data: str,
+                        statusCode: 200,
+                    });
+                });
+            });
+    
+        }
+    },
+    {
 	method: 'GET',
-	path: '/books',
+	path: '/books/ratings',
 	handler: (request, reply)=>{
 		let str = '';
 		let jsonObject = {};
-		https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (response)=>{
-			response.setEncoding('utf8');
-			response.on('data', (data)=>{
-				str = str+data;
+
+		https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (outer)=>{
+			outer.setEncoding('utf8');
+			outer.on('data', (data)=>{
+				str = str + data;
 			});
-			response.on('end', ()=>{
-				 jsonObject = JSON.parse(str);
-				 console.log(jsonObject);
+			outer.on('end', ()=>{
+				jsonObject = JSON.parse(str);
+
+				for (let books in jsonObject.books){
+					let id = jsonObject.books[books].id;
+					https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/'+id, (inner)=>{
+						let str1 = '';
+						let obj = {};
+						inner.setEncoding('utf8');
+						inner.on('data', (data)=>{
+							str1 = str1 + data;
+						});
+						inner.on('end', ()=>{
+							obj = JSON.parse(str1);
+							console.log(obj);
+						});
+					});
+				}
+
 				reply({
-					data: str,
+					data: jsonObject,
 					statusCode: 200,
 				});
+
 			});
 		});
-
 	}
 }];
--- src/server.test.js
+++ src/server.test.js
@@ -2,10 +2,23 @@ const Server = require('./server');
 const Routes = require('../routes');
 
 describe('Testing the Hapi server that processes the requests', () => {
+
     test('Should return 200 status code for sucessful GET request', (done) => {
         const request = {
           method: 'GET',
           url: '/books',
+        };
+        Server.inject(request, (response) => {
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+    });
+
+
+    test('Should return 200 status code for sucessful GET request', (done) => {
+        const request = {
+          method: 'GET',
+          url: '/books/ratings',
         };
         Server.inject(request, (response) => {
           expect(response.statusCode).toBe(200);
